a897cd4966d1decec44282142f1cde316f6207d6
Unknown <lucafloreau@akeonet.com>
2018-12-22 13:17:37 +0100

    Interieur /Exterieur

---
+++ Shostakovich/Assets/Script/Map/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private GameObject room;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (MapManager.Instance.ExteriorPosition)
+        {
+            MapManager.Instance.TransitionInterior(room);
+        }
+        else
+        {
+            MapManager.Instance.TransitionExterior();
+        }
+    }
+}
