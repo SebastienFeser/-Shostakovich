32091c072d55ba915f1621403f7da809e02950ea
Unknown <lucafloreau@akeonet.com>
2018-12-24 17:01:33 +0100

    Mise en place des objet

---
+++ Shostakovich/Assets/Script/Other/FixObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixObject : MonoBehaviour
+{
+    [SerializeField] private bool interactive = false;
+    public bool Interactive => interactive;
+    [SerializeField] private bool wall = true;
+    public bool Wall => wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Shostakovich/Assets/Script/Player/Detector.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour
+{
+
+    private GameObject detectedGameObject;
+
+
+    private BoxCollider2D box;
+    [SerializeField] private LayerMask raycastLayerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        box = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool DetectWall()
+    {
+        
+        Collider2D collider = Physics2D.OverlapBox((Vector2)transform.position + box.offset, box.size, 0, raycastLayerMask);
+        Debug.Log(collider);
+
+        if (collider)
+        {
+            if (collider.GetComponent<FixObject>())
+            {
+                return collider.GetComponent<FixObject>().Wall;
+            }
+        }
+
+        return false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        detectedGameObject = other.gameObject;
+        Debug.Log("detected");
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        detectedGameObject = null;
+    }
+}
