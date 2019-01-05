0ded0c0a5e8681e772d143f89145e42811f99672
Unknown <lucafloreau@akeonet.com>
2019-01-05 15:35:11 +0100

    menu + son

---
+++ Shostakovich/Assets/Script/PNJ/Nikolai.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nikolai : FixObject
+{
+    [SerializeField] private string searchObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Interaction()
+    {
+
+        if (!GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerTest>().SearchInInventory(searchObject))
+        {
+            Dialog.ShowDialog();
+        }
+        else
+        {
+            Dialog.ShowAlternativeDialog();
+        }
+    }
+}
---
+++ Shostakovich/Assets/Script/Props/Leaf.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaf : FixObject
+{
+    [SerializeField] private bool objectContain;
+    [SerializeField] private string objectName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Interaction()
+    {
+        if (objectContain)
+        {
+            Dialog.ShowAlternativeDialog();
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerTest>().NewInventory(objectName);
+        }
+        else
+        {
+            Dialog.ShowDialog();
+           
+        }
+    }
+}
